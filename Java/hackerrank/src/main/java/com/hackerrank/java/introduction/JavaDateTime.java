691a7030eb2da718ce916b6cf9c96f91cf164032
eballo <enric.ballo@gmail.com>
2020-01-24 13:36:59 +0100

    add JavaDateTime

---
+++ Java/hackerrank/src/main/java/com/hackerrank/java/introduction/JavaDateTime.java
@@ -0,0 +1,47 @@
+package com.hackerrank.java.introduction;
+
+import java.io.*;
+import java.time.LocalDate;
+
+public class JavaDateTime {
+
+    /*
+     * Complete the 'findDay' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER month
+     *  2. INTEGER day
+     *  3. INTEGER year
+     */
+
+    public static String findDay(int month, int day, int year) {
+        LocalDate dt = LocalDate.of(year, month, day);
+        return dt.getDayOfWeek().name();
+    }
+
+
+}
+
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int month = Integer.parseInt(firstMultipleInput[0]);
+
+        int day = Integer.parseInt(firstMultipleInput[1]);
+
+        int year = Integer.parseInt(firstMultipleInput[2]);
+
+        String res = JavaDateTime.findDay(month, day, year);
+
+        bufferedWriter.write(res);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

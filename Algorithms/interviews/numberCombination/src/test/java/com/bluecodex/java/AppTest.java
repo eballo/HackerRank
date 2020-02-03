577f91fdaa6ba2838693e1ad2c817856bb5e2514
eballo <enric.ballo@gmail.com>
2020-02-03 19:21:13 +0100

    add interview exercise at TravelPerk

---
+++ Algorithms/interviews/numberCombination/src/test/java/com/bluecodex/java/AppTest.java
@@ -0,0 +1,54 @@
+package com.bluecodex.java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AppTest 
+{
+    @Test
+    public void solution1() {
+
+        List<Integer> listNumbers = Arrays.asList(1,2,3);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        App.solution(listNumbers, 2 , 0, listNumbers.size(), temp, result);
+
+        System.out.println(result);
+
+        Assert.assertEquals("[[1, 2], [1, 3], [2, 3]]", result.toString());
+    }
+
+
+    @Test
+    public void solution2() {
+
+        List<Integer> listNumbers = Arrays.asList(1,2,3,4);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        App.solution(listNumbers, 2 , 0, listNumbers.size(), temp, result);
+
+        System.out.println(result);
+
+        Assert.assertEquals("[[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]", result.toString());
+    }
+
+    @Test
+    public void solution3() {
+
+        List<Integer> listNumbers = Arrays.asList(1,2,3);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        App.solution(listNumbers, 3 , 0, listNumbers.size(), temp, result);
+
+        System.out.println(result);
+
+        Assert.assertEquals("[[1, 2, 3]]", result.toString());
+    }
+}

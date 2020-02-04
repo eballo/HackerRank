84a9c2d7cb80b4ec989645118cb395f7d82bbd37
eballo <enric.ballo@gmail.com>
2020-02-04 17:22:36 +0100

    Interview Preparation kit

---
+++ InterviewPreparationKit/interviewpreparationkit/src/test/java/com/bluecodex/java/interviewkit/SockMerchantTest.java
@@ -0,0 +1,26 @@
+package com.bluecodex.java.interviewkit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SockMerchantTest
+{
+
+    @Test
+    public void sockMerchant1()
+    {
+        int [] ar = {1,2,1,2,1,3,2};
+        int numberOfPairs = SockMerchant.sockMerchant(7, ar);
+
+        Assert.assertEquals(2, numberOfPairs);
+    }
+
+    @Test
+    public void sockMerchant2()
+    {
+        int [] ar = {10,20,20,10,10,30,50,10,20};
+        int numberOfPairs = SockMerchant.sockMerchant(9, ar);
+
+        Assert.assertEquals(3, numberOfPairs);
+    }
+}

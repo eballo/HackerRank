ea2492c67c2ea1b3c56eb805576b20d71d987fad
eballo <enric.ballo@gmail.com>
2020-02-04 10:11:14 +0100

    add Data Structures

--- Algorithms/interviews/numberCombination/src/test/java/com/bluecodex/java/AppTest.java
+++ Algorithms/interviews/numberCombination/src/test/java/com/bluecodex/java/AppTest.java
@@ -51,4 +51,19 @@ public class AppTest
 
         Assert.assertEquals("[[1, 2, 3]]", result.toString());
     }
+
+    @Test
+    public void solution4() {
+
+        List<Integer> listNumbers = Arrays.asList(1,2,3,4,5);
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+
+        App.solution(listNumbers, 3 , 0, listNumbers.size(), temp, result);
+
+        System.out.println(result);
+
+        Assert.assertEquals("[[1, 2, 3], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]", result.toString());
+    }
+
 }

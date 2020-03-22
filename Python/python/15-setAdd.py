fb657a19118f9160d3204ab8050d5f3789960414
eballo <enric.ballo@gmail.com>
2020-03-22 19:10:37 +0100

    15 - set add - python

---
+++ Python/python/15-setAdd.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+
+    n = int(input())
+
+    countries = set()
+    for x in range(n):
+        countries.add(input())
+
+    #print(countries)
+    print(len(countries))
